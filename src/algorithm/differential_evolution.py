096ac20033260e3a47f723232d2a961cee0b922d
Jan Stampfli <jan.stampfli@hslu.ch>
2020-10-19 16:54:17 +0200

    Added differential evolution algorithm (needs testing)

--- src/algorithm/differential_evolution.py
+++ src/algorithm/differential_evolution.py
@@ -10,7 +10,7 @@ class DifferentialEvolution():
     """Differential evolution (DE) algorithm for optimization of heat duties for from genetic algorithm predefined
     HEX matches"""
 
-    def __init__(self, case_study):
+    def __init__(self, case_study, algorithm_parameter):
         self.case_study = case_study
         self.number_heat_exchangers = case_study.number_heat_exchanger
         self.range_heat_exchangers = case_study.range_heat_exchanger
@@ -22,6 +22,13 @@ class DifferentialEvolution():
         self.cold_streams = case_study.cold_streams
         self.min_heat_load = case_study.manual_parameter['MinimalHeatLoad'].iloc[0]
         self.penalty_total_annual_cost_value = case_study.manual_parameter['GA_TAC_Penalty'].iloc[0]
+
+        self.population_size = algorithm_parameter.differential_evolution_population_size
+        self.hall_of_fame_size = algorithm_parameter.differential_evolution_hall_of_fame_size
+        self.number_generations = algorithm_parameter.differential_evolution_number_generations
+        self.number_no_improvement = algorithm_parameter.differential_evolution_number_no_improvement
+        self.probability_crossover = algorithm_parameter.differential_evolution_probability_crossover
+        self.scaling_factor = algorithm_parameter.differential_evolution_scaling_factor
 
     def initialize_individual(self, individual_class, exchanger_addresses):
         """Create an individual matrix of heat duties for all existing HEX matches"""
@@ -49,3 +56,52 @@ class DifferentialEvolution():
 
             fitness = 1 / (self.penalty_total_annual_cost_value + quadratic_distance)
         return fitness,
+
+    def differential_exolution(self, exchanger_addresses):
+        """Main differential evolution algorithm"""
+        toolbox = base.Toolbox()
+        toolbox.register('individual_de', self.initialize_individual, creator.Individual_de)
+        toolbox.register('population_de', tools.initRepeat, list, toolbox.inidividual_de)
+        toolbox.register('select_parents_de', tools.selRandom, k=3)
+        toolbox.register('evaluate_de', self.fitness_function)
+
+        # Initialize population
+        population = toolbox.population_de(n=self.algorithm_parameter.population_size)
+        hall_of_fame_de = tools.HallOfFame(maxsize=self.hall_of_fame_size, similar=np.array_equal)
+        # Evaluate entire population
+        fitness = list(toolbox.map(toolbox.evaluate_de, population))
+        for individual, fit in zip(population, fitness):
+            individual.fitness.values = fit
+
+        number_generation_de = 0
+        number_without_improvement_de = 0
+        while number_generation_de <= self.number_generations and number_without_improvement_de <= self.number_no_improvement:
+            for pop, agent in enumerate(population):
+                individual_r1, individual_r2, individual_r3 = np.array(toolbox.select_parents_de(population))
+                individual_donor = toolbox.clone(agent)
+                index = rng.choice(len(individual_r1))
+                for exchanger in self.range_heat_exchangers:
+                    for operating_case in self.range_operating_cases:
+                        # recombination / Crossover
+                        if pop == index or rng.random() < self.probability_crossover:
+                            # Mutation
+                            individual_donor[exchanger][operating_case] = np.absolute(individual_r1[exchanger][operating_case] + self.scaling_factor * (individual_r2[exchanger][operating_case] - individual_r3[exchanger][operating_case]))
+                            max_heat_duty = np.nanmin((self.hot_streams[exchanger_addresses[exchanger, 0], operating_case], self.cold_streams[exchanger_addresses[exchanger, 1], operating_case]))
+                            if max_heat_duty != 0 and (individual_donor[exchanger][operating_case] < self.min_heat_load or individual_donor[exchanger][operating_case] > max_heat_duty):
+                                individual_donor[exchanger][operating_case] = rng.standard_normal(self.min_heat_load, max_heat_duty)
+                individual_donor.fittness.values = toolbox.evaluate_de(individual_donor)
+                # Selection (objective 1/TAC)
+                if individual_donor[0] > agent.fitness.values[0]:
+                    population[pop] = individual_donor
+            if number_generation_de > 1:
+                best_old = hall_of_fame_de[0].fitness.values[0]
+                hall_of_fame_de.update(population)
+                best_new = hall_of_fame_de[0].fitness.values[0]
+                if best_old >= best_new:
+                    number_without_improvement_de += 1
+                else:
+                    number_without_improvement_de = 0
+            else:
+                hall_of_fame_de.update(population)
+        self.de_best_solution = hall_of_fame_de[0]
+        _ = toolbox.evaluate_de(hall_of_fame_de[0])
